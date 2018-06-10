86c8912172836fd3f3d75d1ca12d24489df29f96
Shibani T <stim599@aucklanduni.ac.nz>
2018-06-10 18:48:07 +1200

    exports a csv with analysed tweets based on how positive/negative the sentiment is and whether it is opinionated

--- twitter-sentiment-analysis/analyzer.py
+++ twitter-sentiment-analysis/analyzer.py
@@ -1,13 +1,22 @@
 from textblob import TextBlob
-import csv
+import csv,sys
+reload(sys)
+sys.setdefaultencoding("ISO-8859-1")
 
-with open('./global_warming.csv', 'rU') as csvfile:
-    readCSV = csv.DictReader(csvfile, delimiter=',')
-    next(readCSV)
-    try:
+POLARITY_THRESHOLD = 0.55;
+SUBJECTIVENESS_THRESHOLD = 0.55;
+
+with open('./analysed_tweets.csv', 'wb') as resultExport:
+    with open('./global_warming.csv', 'rU') as source:
+        readCSV = csv.DictReader(source, delimiter=',')
+        next(readCSV)
+        headers = ['Tweet', 'Sentiment', 'Opinionated']
+        outCSV = csv.DictWriter(resultExport, fieldnames=headers);
+        outCSV.writeheader();
+
         for d in readCSV:
             analysis = TextBlob(d['tweet'])
-            print(analysis.sentiment)
-    except:
-        print("")
-        
+            print(d['tweet'],analysis.sentiment)
+            outCSV.writerow({'Tweet': d['tweet'],
+                             'Sentiment':  'Positive' if analysis.sentiment[0]>POLARITY_THRESHOLD else 'Negative',
+                             'Opinionated': 'Yes' if analysis.sentiment[1]>SUBJECTIVENESS_THRESHOLD else 'No'})
